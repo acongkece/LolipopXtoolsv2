4a7fa2f2681d2370fa72bd68e58889de6f165388
acongkece <stokkcstore@gmail.com>
2024-08-22 15:36:33 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -7,7 +7,7 @@ echo "1. Traker No"                 echo "5. tiktok scrape"
 echo "2. Find User"                 echo "6. Track Nik"
 echo "3. ddos website"              echo "7. getnik"
 echo "4. Instagram Scrape"          echo
-echo "3. Keluar"
+echo "0. Keluar"
 
 read -p "Masukkan pilihan Anda: " choice
 
