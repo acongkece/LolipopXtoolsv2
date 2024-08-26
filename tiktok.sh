f7f919236f52512d04ca51130b02e47b53484a2b
acongkece <stokkcstore@gmail.com>
2024-08-26 17:00:10 +0700

    tiktok.sh

--- tiktok.sh
+++ tiktok.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Fungsi untuk melacak informasi profil TikTok
 function track_tiktok() {
     echo "Masukkan username TikTok:"
     read username
     url="https://www.tiktok.com/@$username"
     
-    # Ambil halaman profil TikTok
     profile_page=$(curl -s $url)
     
-    # Ekstrak informasi dasar
     followers=$(echo "$profile_page" | grep -oP '"followerCount":\K\d+')
     likes=$(echo "$profile_page" | grep -oP '"heartCount":\K\d+')
     videos=$(echo "$profile_page" | grep -oP '"videoCount":\K\d+')
@@ -20,5 +17,4 @@ function track_tiktok() {
     echo "Video: $videos"
 }
 
-# Panggil fungsi track_tiktok langsung
 track_tiktok
