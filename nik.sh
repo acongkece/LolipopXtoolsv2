333b204453b5931446aa965cdd120704f4715689
acongkece <stokkcstore@gmail.com>
2024-08-26 17:26:15 +0700

    nik.sh

--- nik.sh
+++ nik.sh
@@ -3,19 +3,19 @@ function parse_nik() {
     if [[ $nik =~ ^[0-9]{16}$ ]]; then
         echo "NIK valid: $nik"
         
-        # Ekstrak informasi dari NIK
+        
         local province=${nik:0:2}
         local city=${nik:2:2}
         local district=${nik:4:2}
         local birthdate=${nik:6:6}
         local unique_code=${nik:12:4}
         
-        # Parsing tanggal lahir
+        
         local day=${birthdate:0:2}
         local month=${birthdate:2:2}
         local year=${birthdate:4:2}
         
-        # Jika hari lebih dari 40, berarti jenis kelamin perempuan
+    
         if (( day > 40 )); then
             day=$((day - 40))
             gender="Perempuan"
@@ -23,7 +23,7 @@ function parse_nik() {
             gender="Laki-laki"
         fi
         
-        # Asumsi tahun lahir antara 1900-1999
+        
         year="19$year"
         
         echo "Provinsi: $province"
