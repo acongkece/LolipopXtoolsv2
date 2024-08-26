9356750ef39f3cd81d8d6a7f2c1573226157e9f5
acongkece <stokkcstore@gmail.com>
2024-08-26 17:24:05 +0700

    osint1.sh

--- osint1.sh
+++ osint1.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Meminta input username dari pengguna
 read -p "Masukkan username: " username
 
-# Daftar situs yang akan diperiksa
 sites=(
     "https://www.github.com/$username"
     "https://www.twitter.com/$username"
@@ -12,11 +10,34 @@ sites=(
     "https://www.facebook.com/$username"
     "https://www.tiktok.com/$username"
     "https://www.id.pinterest.com/$username"
+    "https://www.facebook.com/$username"
+    "https://www.twitter.com/$username"
+    "https://www.instagram.com/$username"
+    "https://www.linkedin.com/$username"
+    "https://www.github.com/$username"
+    "https://www.pinterest.com/$username"
+    "https://www.tumblr.com/$username"
+    "https://www.youtube.com/$username"
+    "https://soundcloud.com/$username"
+    "https://www.snapchat.com/$username"
+    "https://www.tiktok.com/$username"
+    "https://www.behance.net/$username"
+    "https://www.medium.com/$username"
+    "https://www.quora.com/$username"
+    "https://www.flickr.com/$username"
+    "https://www.periscope.tv/$username"
+    "https://www.twitch.tv/$usernams"
+    "https://www.dribbble.com/$username"
+    "https://www.stumbleupon.com/$username"
+    "https://www.ello.co/$username"
+    "https://www.producthunt.com/$usernams"
+    "https://www.snapchat.com/$username"
+    "https://www.telegram.me/$username"
+    "https://www.weheartit.com/$username"
 )
 
 echo "Mencari informasi untuk username: $username"
 
-# Memeriksa setiap situs
 for site in "${sites[@]}"; do
     response=$(curl -s -o /dev/null -w "%{http_code}" "$site")
     if [ $response -eq 200 ]; then
